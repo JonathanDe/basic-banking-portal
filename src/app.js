777a4a8265550027e250cab3c97f1335fb984e4f
JonathanDe <jonathand9295@gmail.com>
2019-09-17 11:04:39 -0500

    add basic files

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
+const app = express();
+
+const { accounts, users, writeJSON } = require('./data');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Account Summary', accounts})
+});
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {title: 'Account Profile', user: users[0]})
+});
+
+app.listen(3000, () => console.log('PS Project Running on port 3000!'));
